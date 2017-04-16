db372587de9092958eb1794ee10d48342a564a06
Kevin Dennis <kevindennis@mail.usf.edu>
2017-04-16 15:46:49 -0400

    Added a fix for issue on testing

--- app/Database.php
+++ app/Database.php
@@ -135,7 +135,7 @@ class Database extends Model
           print_r("Could not find " . $attribute->name . " using " . $attribute->pivot->column . "<br/><br/>");
         }
 
-        if($attribute->pivot->message_column != "" && !isset($results[0]{$attribute->pivot->column}))
+        if($attribute->pivot->message_column != "" && !isset($results[0]->{$attribute->pivot->column}))
         {
           $fail = true;
           print_r("Could not find " . $attribute->name . " message using " . $attribute->pivot->message_column . "<br/><br/>");
